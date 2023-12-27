1b62137d40bb383f8728150fc263fd0b8a4e1aa7
Phil Dyer <phildyer@protonmail.com>
2023-12-27 16:51:31 +1000

    ryzen_smu: move monitor-cpu into ryzen_smu package

--- pkgs/os-specific/linux/ryzen-smu/default.nix
+++ pkgs/os-specific/linux/ryzen-smu/default.nix
@@ -1,42 +1,74 @@
-{ lib, stdenv, fetchFromGitLab, kernel, linuxHeaders }:
+{ lib
+, stdenv
+, fetchFromGitLab
+, fetchurl
+, kernel
+}:
 
-stdenv.mkDerivation (finalAttrs: {
-  pname = "ryzen_smu-${kernel.version}";
-  version = "0.1.5";
-
-  src = fetchFromGitLab {
+let
+  smu_version = "0.1.5";
+  smu_src = fetchFromGitLab {
     owner = "mann1x";
     repo = "ryzen_smu";
     rev = "adaf53c77dc68b99643ab7af729eb9c592cf5e01";
     hash = "sha256-OQyPqNL8Cd7XQfY8YiOVYqOG7RIu5l+wJoK2FDBg5eg=";
   };
 
+  monitor-cpu = stdenv.mkDerivation (finalAttrs: {
+    pname = "monitor_cpu";
+    version = smu_version;
+
+    src = smu_src;
+
+    makeFlags = [
+      "-C userspace"
+    ];
+
+    installPhase = ''
+    runHook preInstall
+
+    install userspace/monitor_cpu -Dm755 -t $out/bin
+
+    runHook postInstall
+  '';
+
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ryzen_smu-${kernel.version}";
+  version = smu_version;
+
+  src = smu_src;
+
+  hardeningDisable = [ "pic" ];
+
+  nativeBuildImputs = kernel.moduleBuildDependencies;
+
   buildInputs = [
     linuxHeaders
   ];
 
   makeFlags = [
-    "KERNEL_MODULES=${kernel.dev}/lib/modules/${kernel.modDirVersion}"
+    "TARGET=${kernel.modDirVersion}"
+    "KERNEL_BUILD=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
   ];
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p "$out/lib/modules/${kernel.modDirVersion}"
-
-    for module in ryzen_smu.ko; do
-        mv $module $out/lib/modules/${kernel.modDirVersion}
-    done
+    install ryzen_smu.ko -Dm444 -t $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/ryzen_smu
+    install ${monitor_cpu}/bin/monitor_cpu -Dm755 -t $out/bin
 
     runHook postInstall
   '';
 
-  meta = {
+  meta = with lib; {
     broken = stdenv.isAarch64;
     description = "A Linux kernel driver that exposes access to the SMU (System Management Unit) for certain AMD Ryzen Processors";
     homepage = "https://gitlab.com/leogx9r/ryzen_smu";
-    license = lib.licenses.gpl2Plus;
-    maintainers = [ lib.maintainers.phdyellow ];
-    platforms = lib.platforms.linux;
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ Cryolitia phdyellow ];
+    platforms = platforms.linux;
+    mainProgram = "monitor_cpu";
+
   };
 })
