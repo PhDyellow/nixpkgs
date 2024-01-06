cdf5a0a34fd818177d1cec9a7e9246f0bbde92ce
Phil Dyer <phildyer@protonmail.com>
2024-01-06 12:38:34 +1000

    ryzen_smu: remove package option

--- nixos/modules/hardware/cpu/amd-ryzen-smu.nix
+++ nixos/modules/hardware/cpu/amd-ryzen-smu.nix
@@ -4,6 +4,7 @@ with lib;
 
 let
   cfg = config.hardware.cpu.amd.ryzen_smu;
+  ryzen_smu = config.boot.kernelPackages.ryzen_smu;
 in
 {
   options.hardware.cpu.amd.ryzen_smu = {
@@ -12,17 +13,12 @@ in
 
         WARNING: Damage cause by use of your AMD processor outside of official AMD specifications or outside of factory settings are not covered under any AMD product warranty and may not be covered by your board or system manufacturer's warranty.
       '');
-
-    package = mkPackageOption pkgs.linuxPackages "ryzen_smu" {
-      pkgsText = "pkgs.linuxPackages";
-    };
-
   };
 
   config = mkIf cfg.enable {
-    boot.kernelModules = [ "${cfg.package.name}" ];
-    boot.extraModulePackages = [ cfg.package ];
-    environment.systemPackages = [ cfg.package ];
+    boot.kernelModules = [ "ryzen_smu" ];
+    boot.extraModulePackages = [ ryzen_smu ];
+    environment.systemPackages = [ ryzen_smu ];
   };
 
   meta.maintainers = with maintainers; [ Cryolitia phdyellow ];
