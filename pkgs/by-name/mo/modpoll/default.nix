070232538cddb6254e0beb3e948c0dcc9d3d7545
Phil Dyer <phildyer@protonmail.com>
2026-04-13 15:44:00 +1000

    modpoll: init at 1.6.0
    Modpoll - A New Command-line Tool for Modbus and MQTT

---
+++ pkgs/by-name/mo/modpoll/default.nix
@@ -0,0 +1,53 @@
+{ lib
+, python3Packages
+, fetchFromGitHub
+}:
+
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "modpoll";
+  version = "1.6.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "gavinying";
+    repo = "${finalAttrs.pname}";
+    rev = "v${finalAttrs.version}";
+    hash = lib.fakeHash;
+  };
+
+  build-system = with python3Packages; [
+    poetry-core
+
+  ];
+
+  dependencies = with python3Packages; [
+    pymodbus
+    paho-mqtt
+    prettytable
+    requests
+    pyserial
+
+    # requirements.txt not in above
+    certifi
+    charset-normalizer
+    idna
+    urllib3
+    wcwidth
+
+    # pyproject dev dependencies
+    pre-commit
+    pytest
+    deptry
+    tox
+  ];
+
+  meta = {
+    changelog = "https://github.com/gavinying/modpoll/releases/tag/v${finalAttrs.version}";
+    description = "Modpoll - A New Command-line Tool for Modbus and MQTT";
+    homepage = "https://github.com/gavinying/modpoll";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      phdyellow
+    ];
+  };
+})
